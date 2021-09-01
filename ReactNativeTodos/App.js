32b06e6a28a8079f45453eb5fd330b4aa41bc3ea
DESKTOP-VF6KUIN\UserK <jswj30@naver.com>
2021-09-01 17:59:48 +0900

    Card View 추가하기

--- ReactNativeTodos/App.js
+++ ReactNativeTodos/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import { SafeAreaView, StyleSheet, TextInput, View, Text } from 'react-native'
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.appTitle}>Hello Todolist</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder="Add an item!" />
+      </View>
     </SafeAreaView>
   );
 };
@@ -12,7 +15,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#3143e8' 
+    backgroundColor: '#3143e8', 
   }, 
   appTitle: {
     color: '#fff', 
@@ -21,7 +24,22 @@ const styles = StyleSheet.create({
     marginBottom: 30, 
     fontWeight: '300', 
     textAlign: 'center', 
-    backgroundColor: '#3143e8'
+    backgroundColor: '#3143e8', 
+  }, 
+  card: {
+    backgroundColor: '#fff', 
+    flex: 1, 
+    borderTopLeftRadius: 10, 
+    borderTopRightRadius: 10, 
+    marginLeft: 10, 
+    marginRight: 10, 
+  }, 
+  input: {
+    padding: 20, 
+    borderBottomColor: '#bbb', 
+    borderBottomWidth: 1, 
+    fontSize: 24, 
+    marginLeft: 20, 
   }, 
 });
 
