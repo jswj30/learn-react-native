8b0483b41d2c60602dc2ccda53a3b9639f21639e
DESKTOP-GJ4SVDS\fljs2 <jswj30@naver.com>
2021-09-05 18:40:03 +0900

    마침내 켜짐

--- ReactNativeTodos/components/TodoListItem.js
+++ ReactNativeTodos/components/TodoListItem.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  View, 
-  Text, 
-  StyleSheet, 
-  TouchableOpacity
-} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 import Icon from 'react-native-vector-icons/AntDesign';
 
@@ -14,18 +9,22 @@ const TodoListItem = ({id, textValue, checked, onRemove, onToggle}) => {
       <TouchableOpacity onPressOut={onToggle(id)}>
         {checked ? (
           <View style={styles.completeCircle}>
-            <Icon name="circledowno" size={30} color='#3143e8' />
+            <Icon name="circledowno" size={30} color="#3143e8" />
           </View>
         ) : (
           <View style={styles.circle} />
         )}
       </TouchableOpacity>
-      <Text style={[styles.text, checked ? styles.strikeText : styles.unstrikeText]}>
+      <Text
+        style={[
+          styles.text,
+          checked ? styles.strikeText : styles.unstrikeText,
+        ]}>
         {textValue}
       </Text>
       <TouchableOpacity style={styles.buttonContainer}>
         <Text style={styles.buttonText} onPress={() => onRemove(id)}>
-          <Icon name="delete" size={30} color='#e33057' />
+          <Icon name="delete" size={30} color="#e33057" />
         </Text>
       </TouchableOpacity>
     </View>
@@ -34,44 +33,45 @@ const TodoListItem = ({id, textValue, checked, onRemove, onToggle}) => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    borderBottomColor: "#bbb", 
-    borderBottomWidth: StyleSheet.hairlineWidth, 
-    flexDirection: 'row', 
-    alignItems: 'center', 
-    justifyContent: 'space-between', 
-  }, 
+    flex: 1,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
   circle: {
-    width: "30rem", 
-    height: 30, 
-    borderRadius: 15, 
-    borderColor: 'blue', 
-    borderWidth: 2, 
-    marginLeft: 20, 
-    marginRight: 20, 
-  }, 
+    // width: '30rem',
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderColor: 'blue',
+    borderWidth: 2,
+    marginLeft: 20,
+    marginRight: 20,
+  },
   text: {
-    flex: 5, 
+    flex: 5,
     fontWeight: '500',
-    fontSize: 18, 
-    marginVertical: 20, 
-    width: 100,  
-  }, 
+    fontSize: 18,
+    marginVertical: 20,
+    width: 100,
+  },
   completeCircle: {
-    marginLeft: 20, 
-    marginRight: 20, 
-  }, 
+    marginLeft: 20,
+    marginRight: 20,
+  },
   strikeText: {
-    color: '#bbb', 
-    textDecorationLine: 'line-through', 
-  }, 
+    color: '#bbb',
+    textDecorationLine: 'line-through',
+  },
   unstrikeText: {
-    color: '#29323c', 
-  }, 
+    color: '#29323c',
+  },
   buttonContainer: {
-    marginVertical: 10, 
-    marginHorizontal: 10, 
-  }, 
+    marginVertical: 10,
+    marginHorizontal: 10,
+  },
 });
 
 export default TodoListItem;
