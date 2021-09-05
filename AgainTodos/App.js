3ea1f74e8d4d29a374ad748706aee2e9a38a01fa
DESKTOP-GJ4SVDS\fljs2 <jswj30@naver.com>
2021-09-05 19:57:37 +0900

    App.js 기본 설정

---
+++ AgainTodos/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello React Native</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 45,
+  },
+});
+
+export default App;
