d1f6edee102458e6e53bae2d31bb5d498b266ec0
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2021-09-10 09:02:57 +0900

    rnTutorial 작동안됨.

--- rnTutorial/App.js
+++ rnTutorial/App.js
@@ -1,35 +1,33 @@
-import React, {Component} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native'
 
 class Home extends Component {
   render() {
     return (
-      <View style={styles.eachView}>
+      <View style={styles.eachView} >
         <Text> 홈 화면 입니다.</Text>
       </View>
-    );
+    )
   }
 }
 
 class Chat extends Component {
   render() {
     return (
-      <View style={styles.eachView}>
+      <View style={styles.eachView} >
         <Text> 챗 화면 입니다.</Text>
       </View>
-    );
+    )
   }
 }
 
 class Settings extends Component {
   render() {
     return (
-      <View style={styles.eachView}>
+      <View style={styles.eachView} >
         <Text> 세팅 화면 입니다.</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -41,32 +39,28 @@ const App = createStackNavigator(
     Home: {
       screen: Home,
     },
-    Settings,
+    Settings // 설정이 'screen' 밖에 없다면, 중괄호는 생략 가능합니다.
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Home' // 처음 보여 줄 화면을 설정합니다.
   },
 );
 
-const AppContainer = createAppContainger;
+const AppContainer = createAppContainer(App);
 
 export default () => (
-  <View style={styles.centerView}>
-    <Home />
-    <Chat />
-    <Settings />
-  </View>
+  <AppContainer />
 );
 
 const styles = StyleSheet.create({
   centerView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   eachView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
