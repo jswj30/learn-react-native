e1e02c878a2a1f775e654ff695eabd4329256685
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2021-09-28 08:28:38 +0900

    3.1

--- ReactNativeComponent/App.js
+++ ReactNativeComponent/App.js
@@ -1,112 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View
+      style={{
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        backgroundColor: '#fff', 
+      }}
+    >
+      <Text>Open up App.js. js to start working on your app!</Text>
     </View>
   );
-};
+}
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+// export default function App() {
+//   // return null;
+//   // return undefined;
+  
+//   const name = 'Beomjun';
+//   return (
+//     <View style={styles.container}>
+//       {name === 'Beomjun' && (
+//         <Text style={styles.text}>My name is Beomjun</Text>
+//       )}
+//       {name !== 'Beomjun' && (
+//         <Text style={styles.text}>My name is not Beomjun</Text>
+//       )}
+//       {/* <Text style={styles.text}>
+//         My name is {name === 'Beomjun' ? "Beomjun Kim" : 'React Native'}
+//       </Text> */}
+//       {/* <Text style={styles.text}>
+//         My name is {name === 'Beomjun' ? "Beomjun Kim" : 'React Native'}
+//       </Text> */}
+//       {/* <Text style={styles.text}>
+//         {
+//           (() => {
+//             if (name === 'Hanbit') return 'My name is Hanbit';
+//             else if(name === 'Beomjun') return "My name is Beomjun";
+//             else return "My name is React Native";
+//           })()
+//         }
+//       </Text> */}
+//     </View>
+//   )
+// }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  container: {
+    flex: 1, 
+    backgroundColor: '#fff', 
+    justifyContent: 'center', 
+    alignItems: 'center'
+  }, 
+  text: {
+    fontSize: 30, 
+  }, 
 });
-
-export default App;
