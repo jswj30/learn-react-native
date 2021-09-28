dbaff14590f4c5fbec27c9145880ba1d879b1b0e
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2021-09-28 12:46:22 +0900

    fade in / out 코드를 함수형으로 변환

--- TimingAnimation/App.js
+++ TimingAnimation/App.js
@@ -1,46 +1,89 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 // Animated 모듈 가져오기
-import { View, Animated } from 'react-native';
+import { StyleSheet, View, Animated, Text } from 'react-native';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // Animated 초기값 지정
-      animation: new Animated.Value(1), 
-    }
-  }
+const App = () => {
+  const [animation, setAnimation] = useState(new Animated.Value(0));
 
-  componentDidMount() {
+  useEffect(() => {
     Animated.timing(
-      this.state.animation, 
+      animation, 
       {
-        toValue: 0, 
+        toValue: 1, 
         duration: 2000, 
       }
     ).start();
-  }
-  
-  
+  }, []);
 
-  render() {
-    const animationStyles = {
-      opacity: this.state.animation, 
-    }
-
-    return (
-      <Animated.View style={[objectStyles.object, animationStyles]}>
-      </Animated.View>
-    );
+  const animationStyles = {
+    opacity: animation, 
   }
+
+  return (
+    <Animated.View style={[styles.object, animationStyles]}>
+      <Text>테스트</Text>
+    </Animated.View>
+  )
 }
 
-const objectStyles = {
+const styles = StyleSheet.create({
   object: {
     backgroundColor: 'orange', 
     width: 100, 
     height: 100, 
   }
-}
+})
 
 export default App;
+
+
+
+
+
+
+// import React, { Component } from 'react';
+// // Animated 모듈 가져오기
+// import { View, Animated } from 'react-native';
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       // Animated 초기값 지정
+//       animation: new Animated.Value(1), 
+//     }
+//   }
+
+//   componentDidMount() {
+//     Animated.timing(
+//       this.state.animation, 
+//       {
+//         toValue: 0, 
+//         duration: 2000, 
+//       }
+//     ).start();
+//   }
+  
+  
+
+//   render() {
+//     const animationStyles = {
+//       opacity: this.state.animation, 
+//     }
+
+//     return (
+//       <Animated.View style={[objectStyles.object, animationStyles]}>
+//       </Animated.View>
+//     );
+//   }
+// }
+
+// const objectStyles = {
+//   object: {
+//     backgroundColor: 'orange', 
+//     width: 100, 
+//     height: 100, 
+//   }
+// }
+
+// export default App;
