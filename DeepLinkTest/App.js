aac25c036f68b130a072a7f44e0dedf97b2edd3c
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2021-09-29 13:01:43 +0900

    1

--- DeepLinkTest/App.js
+++ DeepLinkTest/App.js
@@ -2,16 +2,27 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer, useFocusEffect } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Linking from 'expo-linking';
 
 import HomeScreen from './components/HomeScreen';
 import DetailsScreen from './components/DetailsScreen';
 import BackScreen from './components/BackScreen';
 
+// expo
+// const prefix = Linking.createURL('/');
+
 const App = () => {
+
+  // expo
+  // const linking = {
+  //   prefixes: [prefix], 
+  // }
 
   const Stack = createNativeStackNavigator();
 
   return(
+    // expo
+    // <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
     <NavigationContainer>
       <Stack.Navigator 
         initialRouteName="Home"
