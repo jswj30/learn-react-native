b95c4b0059cb82868f1a80d50d3c895cf3d7d630
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2021-10-16 20:13:34 +0900

    1

--- RNPressable/App.js
+++ RNPressable/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Pressable } from 'react-native';
+import { View } from 'react-native';
 import Button from './components/Button';
 
 const App = () => {
--- RNPressable/components/Button.js
+++ RNPressable/components/Button.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Pressable } from 'react-native';
+import { Text, Pressable } from 'react-native';
 
 const Button = (props) => {
   return (
