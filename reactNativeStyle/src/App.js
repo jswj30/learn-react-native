5131e74b52f5d268eaa04a16fcb9cb752f59bc31
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2021-10-18 19:44:37 +0900

    class styling

--- reactNativeStyle/src/App.js
+++ reactNativeStyle/src/App.js
@@ -1,37 +1,64 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 const App = () => {
   return (
-    <View
-      style={{
-        backgroundColor: '#fff', 
-        alignItems: 'center', 
-        justifyContent: 'center', 
-      }}
-    >
-      <Text
-        style={{
-          padding: 10, 
-          fontSize: 26, 
-          fontWeight: '600', 
-          color: 'black', 
-        }}
-      >
-        React Native Style
-      </Text>
-      <Text
-        style={{
-          padding: 10, 
-          fontSize: 26, 
-          fontWeight: '400', 
-          color: 'red', 
-        }}
-      >
-        Inline Styling - Error
-      </Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>React Native Style</Text>
+      <Text style={styles.error}>Inline Styling - Error</Text>
     </View>
+
+
+    // <View
+    //   style={{
+    //     backgroundColor: '#fff', 
+    //     alignItems: 'center', 
+    //     justifyContent: 'center', 
+    //   }}
+    // >
+    //   <Text
+    //     style={{
+    //       padding: 10, 
+    //       fontSize: 26, 
+    //       fontWeight: '600', 
+    //       color: 'black', 
+    //     }}
+    //   >
+    //     React Native Style
+    //   </Text>
+    //   <Text
+    //     style={{
+    //       padding: 10, 
+    //       fontSize: 26, 
+    //       fontWeight: '400', 
+    //       color: 'red', 
+    //     }}
+    //   >
+    //     Inline Styling - Error
+    //   </Text>
+    // </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flix: 1, 
+    backgroundColor: '#fff', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+  }, 
+  text: {
+    padding: 10, 
+    fontSize: 26, 
+    fontWeight: '600', 
+    color: 'black', 
+  }, 
+  error: {
+    padding: 10, 
+    fontSize: 26, 
+    fontWeight: '400', 
+    color: 'red', 
+  }
+})
 
 export default App;
