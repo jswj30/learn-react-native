3f4aa96ca8c1c7515fe98984e0051a5279681ea1
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2021-10-18 20:00:33 +0900

    import styles

--- reactNativeStyle/src/App.js
+++ reactNativeStyle/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { viewStyles, textStyles } from './styles';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={[styles.text, { color: 'green' }]}>React Native Style</Text>
-      <Text style={[styles.text, styles.error]}>Inline Styling - Error</Text>
+    <View style={viewStyles.container}>
+      <Text style={[textStyles.text, { color: 'green' }]}>React Native Style</Text>
+      <Text style={[textStyles.text, textStyles.error]}>Inline Styling - Error</Text>
     </View>
+
+
+    // <View style={styles.container}>
+    //   <Text style={[styles.text, { color: 'green' }]}>React Native Style</Text>
+    //   <Text style={[styles.text, styles.error]}>Inline Styling - Error</Text>
+    // </View>
 
 
     // <View
@@ -40,29 +47,29 @@ const App = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flix: 1, 
-    backgroundColor: '#fff', 
-    alignItems: 'center', 
-    justifyContent: 'center', 
-  }, 
-  text: {
-    padding: 10, 
-    fontSize: 26, 
-    fontWeight: '600', 
-    color: 'black', 
-  }, 
-  // error: {
-  //   padding: 10, 
-  //   fontSize: 26, 
-  //   fontWeight: '400', 
-  //   color: 'red', 
-  // },
-  error: {
-    fontWeight: '400', 
-    color: 'red', 
-  },  
-})
+// const styles = StyleSheet.create({
+//   container: {
+//     flix: 1, 
+//     backgroundColor: '#fff', 
+//     alignItems: 'center', 
+//     justifyContent: 'center', 
+//   }, 
+//   text: {
+//     padding: 10, 
+//     fontSize: 26, 
+//     fontWeight: '600', 
+//     color: 'black', 
+//   }, 
+//   // error: {
+//   //   padding: 10, 
+//   //   fontSize: 26, 
+//   //   fontWeight: '400', 
+//   //   color: 'red', 
+//   // },
+//   error: {
+//     fontWeight: '400', 
+//     color: 'red', 
+//   },  
+// });
 
 export default App;
