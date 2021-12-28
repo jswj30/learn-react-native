69821b25a9e9f56f02dc7a6274542319eec9ef27
DESKTOP-SCMQBSV\fljs2 <jswj30@naver.com>
2021-12-28 10:19:25 +0900

    init

---
+++ reactNativeNavigation/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styled from 'styled-components';
+
+const App = () => {
+  return <Container>
+  </Container>
+}
+
+export default App;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #ffffff;
+  justify-content: center;
+  align-items: center;
+`;
